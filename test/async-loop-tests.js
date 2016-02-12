ca189cee27d9c5f3fb9c4489b00d2a55659466fe
Will James <wjames@thoughtworks.com>
2016-02-12 11:09:29 +1100

    rename package

--- test/async-loop-tests.js
+++ test/async-loop-tests.js
@@ -1,8 +1,8 @@
 var expect = require('chai').expect;
-var asyncLoop = require('../src/asyncLoop.js');
+var asyncLoop = require('../src/async-loop.js');
 
 
-describe('asyncLoop', function() {
+describe('async.loop', function() {
   it('should run until break is called', function(done) {
     var count = 0;
     asyncLoop(function(_break, _continue) {
